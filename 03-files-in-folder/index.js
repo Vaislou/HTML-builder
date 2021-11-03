a6c80863c38e8c8935a688155a4a3bd8947a87c3
Vaislou <vaislou@gmail.com>
2021-11-03 18:40:36 +0300

    fix: fix warnings (eslint) (Wednesday, November 3, 06:40:12 PM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,8 +7,8 @@ const inside = fs.readdir(url, { withFileTypes: true }, (err, arr) => {
     arr.forEach(item => {
         fs.stat(`${url}/${item.name}`, (err, e) => {
             if (e.isFile()) {
-                console.log(`${item.name.split('.').join(' - ')} - ${e.size / 1000}kb`)
+                console.log(`${item.name.split('.').join(' - ')} - ${e.size / 1000}kb`);
             }
-        })
+        });
     });
 });
