b44563fce116256feea2b276252a05993c7ed322
Vaislou <vaislou@gmail.com>
2021-11-03 20:47:33 +0300

    feat: complite 04-copy-directory (Wednesday, November 3, 08:47:34 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const url = path.join(__dirname, 'files-copy');
+
+fs.mkdir(url, { recursive: true }, () => {
+    fs.readdir(path.join(__dirname, 'files'), (err, arr) => {
+        arr.forEach((item) => {
+            fs.copyFile(path.join(__dirname, `files/${item}`), path.join(__dirname, `files-copy/${item}`), () => {
+                console.log(`File ${item} - copied`);
+            });
+            fs.readdir(url, (error, array) => {
+                array.forEach((copyItem) => {
+                    if (!arr.includes(copyItem)) {
+                        fs.unlink(`${url}/${copyItem}`, () => {
+                            console.log('removed');
+                        });
+                    }
+                });
+            });
+        });
+    });
+});
+
+
 
