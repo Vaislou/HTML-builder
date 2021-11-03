78da9c19050df1effc3efcbf1188da652c494f26
Vaislou <vaislou@gmail.com>
2021-11-03 18:39:38 +0300

    feat: complete 03-files-in-folder (console.log(Wednesday, November 3, 06:39:34 PM)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const url = path.join(__dirname, 'secret-folder');
+
+const inside = fs.readdir(url, { withFileTypes: true }, (err, arr) => {
+    arr.forEach(item => {
+        fs.stat(`${url}/${item.name}`, (err, e) => {
+            if (e.isFile()) {
+                console.log(`${item.name.split('.').join(' - ')} - ${e.size / 1000}kb`)
+            }
+        })
+    });
+});
