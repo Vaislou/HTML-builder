36d2e47bff01a95d2ca73507440bd08856183eaf
Vaislou <vaislou@gmail.com>
2021-11-04 19:14:57 +0300

    feat: add hello and bye to 02 (Thursday, November 4, 07:14:22 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,5 +4,21 @@ const path = require('path');
 const url = path.join(__dirname, 'text.txt');
 
 const writeFile = fs.createWriteStream(url);
+
+console.log('Enter your message')
 process.stdin.pipe(writeFile);
 process.stdin.resume();
+
+process.on('SIGINT', () => {
+    process.stdout.write('Bye');
+    process.exit();
+});
+
+process.stdin.on('data', data => {
+    let a = data.toString().trim();
+    if (a === 'exit') {
+        process.stdout.write('Bye');
+        process.exit();
+    }
+})
+
